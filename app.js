bdc2ba10d0b06effdab1ed0eb3c3cdd950b97a0f
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-03 10:39:40 +0100

    until palindroma

---
+++ app.js
@@ -0,0 +1,24 @@
+// INPUT E OUTPUT HTML
+let parola = document.getElementById('parola')
+let result = document.querySelector('.result')
+let generate = document.querySelector('.btn-secondary')
+
+generate.addEventListener('click', generating)
+
+function ReverseString(str) {
+    return str.split('').reverse().join('')
+}
+function generating() {
+    reset();
+    if (parola.value == ReverseString(parola.value)) {
+        result.append('Questa parola è palindroma')
+    } else {
+        result.append('Questa parola non è palindroma')
+    }
+}
+function reset() {
+    result.innerHTML = ''
+}
+
+        
+
